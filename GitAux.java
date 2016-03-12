dcde6a0512398927b8f3ef2a1e97058670e850e2
sergiotorrestellez@hotmail.com <sergiottellez@MacBook-Pro-de-Sergio.local>
2016-03-12 17:33:31 +0100

    modificaciones alumno 2

--- GitAux.java
+++ GitAux.java
@@ -16,9 +16,11 @@ public class GitAux {
     //Nuevo comentario
     public void metodoAlumno2(){
         System.out.println("metodo 2");
+	System.out.println(“modificado por alumno 2”); // alumno 2
     }
     
     public void metodoComunitario(){
         System.out.println("Aqui escribimos todos");
+	// modificaciones alumno 2        System.out.println("Alumno 2 inserta esta sentencia");
     }
 }
